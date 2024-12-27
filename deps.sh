e75548c20459c3181c5a9fe31bb986b9bbc396ee
Yorick Peterse <yorick@yorickpeterse.com>
2024-12-27 23:45:02 +0100

    Fix building without a network connection
    The resulting Flatpak will crash, but now we can at least build without
    `zig build` downloading dependencies. The next step is to find a way to
    remove the need for bundling the dependencies in a tar archive.

--- deps.sh
+++ deps.sh
@@ -12,6 +12,6 @@ cd ghostty-source
 
 # Fetch the dependencies and remove any large files/directories we don't need.
 ZIG_GLOBAL_CACHE_DIR=vendor ./nix/build-support/fetch-zig-cache.sh
-rm -rf vendor/p/*/{test,Test,screenshots,result,doc,data}
+rm -rf vendor/p/*/{test,Test,screenshots,result,doc}
 tar -cf vendor.tar.gz vendor
 mv vendor.tar.gz ../data/vendor.tar.gz
